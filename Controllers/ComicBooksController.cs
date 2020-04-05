50ae524e3135c62fbe8e580f88d0c500fcd7137a
Nathania Hores <nhores93@gmail.com>
2020-04-05 08:34:22 -0700

    Added comic book repository

---
+++ Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using ComicBookGallery.Models;
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
